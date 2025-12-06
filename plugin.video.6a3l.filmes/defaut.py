a85b32f538e637f10c4078bfdec2ecfb4ac60eae
gaeloj <oficialgaeloj@gmail.com>
2025-12-06 01:55:41 -0300

    Update defaut.py

--- plugin.video.6a3l.filmes/defaut.py
+++ plugin.video.6a3l.filmes/defaut.py
@@ -1,1 +1,13 @@
+import xbmcplugin
+import xbmcgui
+import sys
+
+handle = int(sys.argv[1])
+
+# Item de vídeo do Google Drive
+li = xbmcgui.ListItem("Vídeo de teste")
+url = "https://drive.google.com/uc?export=download&id=19ZVWLWZXKJJZUmr0KRmfehoFIPBNmahL"
+xbmcplugin.addDirectoryItem(handle=handle, url=url, listitem=li)
+
+xbmcplugin.endOfDirectory(handle)
 
